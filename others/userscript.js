83293e7974c738167eb2081e876f59c90fee0530
I-Ta Tsai <ittsai@gmail.com>
2024-05-07 10:24:26 +0800

    merge

--- others/userscript.js
+++ others/userscript.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Enhanced WebRIS
 // @namespace    http://tsai.it/
-// @version      2024-05-06a
+// @version      2024-05-07a
 // @description  try to take over the world!
 // @author       You
 // @match        http://10.2.2.160:8080/
@@ -262,6 +262,7 @@
 
         'C spine dynamic': ['C spine AP', 'C spine AP+ lateral'],
         'C spine AP': ['C spine dynamic', 'C spine AP+ lateral'],
+        'C spine AP+ lateral': ['C spine dynamic', 'C spine AP'],
         'T spine AP+lateral': ['TL spine AP+lateral', 'TL spine AP+lateral standing', 'T-L spine AP+Lat'],
         'TL spine AP+lateral': ['TL spine AP+lateral standing', 'T-L spine AP+Lat', 'LS spine lateral standing'],
         'LS spine AP+lateral': [
@@ -288,6 +289,8 @@
         'Elbow AP+lateral(R)': ['Elbow oblique(R)'],
         'Elbow oblique(L)': ['Elbow AP+lateral(L)'],
         'Elbow AP+lateral(L)': ['Elbow oblique(L)'],
+        'Knee AP+lateral(R)': ['JOINT Knee CT'],
+        'Knee AP+lateral(L)': ['JOINT Knee CT'],
         'Pelvis THR': ['Hip lateral(R)', 'Hip lateral(L)', 'KUB'],
         'Hip lateral(R)': ['Pelvis THR'],
         'Hip lateral(L)': ['Pelvis THR'],
