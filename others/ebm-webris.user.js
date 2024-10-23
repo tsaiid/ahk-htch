1363578ded4a0ffd6a984e81959d0c1a77e7c47f
I-Ta Tsai <ittsai@gmail.com>
2024-10-23 10:35:35 +0800

    update

--- others/ebm-webris.user.js
+++ others/ebm-webris.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Enhanced WebRIS
 // @namespace    http://tsai.it/
-// @version      20241021.3
+// @version      20241023.1
 // @description  Add more functions and colors to EBM WebRIS
 // @author       I-Ta Tsai
 // @match        http://10.2.2.160:8080/
@@ -131,7 +131,8 @@
         return examName.match(/JOINT|Leg|Hand-CT/);
     }
     function isAngio(examName) {
-        const angioList = ['Celiac a three vessel', 'Lipiodol T.A.E.(trans-arterial embolization)-Lipiodol'];
+        const angioList = ['Celiac a three vessel', 'Lipiodol T.A.E.(trans-arterial embolization)-Lipiodol',
+                           'Percutaneous placement of ureter stent', 'Percutaneous retrieval of ureteral stent'];
         return angioList.includes(examName);
     }
     function isSonoCDU(examName) {
