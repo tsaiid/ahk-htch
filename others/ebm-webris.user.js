254dc6b4f05d43b2c027be09c33167c55f8ae199
I-Ta Tsai <ittsai@gmail.com>
2024-12-17 22:45:47 +0800

    update

--- others/ebm-webris.user.js
+++ others/ebm-webris.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Enhanced WebRIS
 // @namespace    http://tsai.it/
-// @version      20241217.3
+// @version      20241217.4
 // @description  Add more functions and colors to EBM WebRIS
 // @author       I-Ta Tsai
 // @match        http://10.2.2.160:8080/
@@ -118,7 +118,8 @@
 
     function isAbdCT(examName) {
         const abdCTList = ['Abdomen to Pelvis CT', 'Abdomen  Liver Triple Phase CT', 'Abdomen  Liver 4 Phase CT',
-                           'ABDOMEN  Lymph  Nodes   CT', 'Colon cancer (Abdomen & Pelvis)-CT'];
+                           'ABDOMEN  Lymph  Nodes   CT', 'Colon cancer (Abdomen & Pelvis)-CT',
+                           'Abdomen pancreas dual phase CT'];
         return abdCTList.includes(examName);
     }
     function isChestCT(examName) {
