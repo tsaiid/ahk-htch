52c659a2f354e080bd6db70a2ec182c61a9462a5
I-Ta Tsai <ittsai@gmail.com>
2025-02-20 18:47:36 +0800

    update

--- others/ebm-webris.user.js
+++ others/ebm-webris.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Enhanced WebRIS
 // @namespace    http://tsai.it/
-// @version      20250217.1
+// @version      20250220.1
 // @description  Add more functions and colors to EBM WebRIS
 // @author       I-Ta Tsai
 // @match        http://10.2.2.160:8080/
@@ -867,6 +867,7 @@
         'JOINT Wrist MRI': ['Wrist PA+lateral(R)', 'Wrist PA+lateral(L)'],
 
         'ABDOMEN Liver MRI': ['Abdomen  Liver 4 Phase CT', 'Abdomen  Liver Triple Phase CT'],
+        'Liver (Primovist) MRI': ['ABDOMEN Liver MRI', 'Abdomen  Liver 4 Phase CT', 'Abdomen  Liver Triple Phase CT'],
         'Colon cancer (Abdomen & Pelvis) MRI': ['Colon cancer (Abdomen & Pelvis)-CT'],
         'PELVIS Prostate MRI': ['Prostate cancer (Abdomen & Pelvis) MRI'],
         'Prostate cancer (Abdomen & Pelvis) MRI': ['PELVIS Prostate MRI'],
