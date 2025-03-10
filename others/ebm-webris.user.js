950f905ee109bc8bfe91ecafd613ed5c89c483b9
I-Ta Tsai <ittsai@gmail.com>
2025-03-10 15:38:43 +0800

    fix: trim string for possible untrimmed data

--- others/ebm-webris.user.js
+++ others/ebm-webris.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Enhanced WebRIS
 // @namespace    http://tsai.it/
-// @version      20250308.1
+// @version      20250310.1
 // @description  Add more functions and colors to EBM WebRIS
 // @author       I-Ta Tsai
 // @match        http://10.2.2.160:8080/
@@ -50,7 +50,7 @@
                 const parent = label.parentElement;
                 const input = parent.querySelector('input');
                 if (input) {
-                    return input.value;
+                    return input.value.trim();
                 }
             }
         }
