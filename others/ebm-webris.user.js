5848f3f2d7c1beb975013b152017363896cb2ddd
I-Ta Tsai <ittsai@gmail.com>
2025-06-02 14:18:27 +0800

    update

--- others/ebm-webris.user.js
+++ others/ebm-webris.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Enhanced WebRIS
 // @namespace    http://tsai.it/
-// @version      20250528.1
+// @version      20250602.1
 // @description  Add more functions and colors to EBM WebRIS
 // @author       I-Ta Tsai
 // @match        http://10.2.2.160:8080/
@@ -148,7 +148,7 @@
         return examName.includes("Aorta");
     }
     function isMsk(examName) {
-        return examName.match(/JOINT|Leg|Hand-CT|Forearm- CT|Femur|foot (CT|MRI)/);
+        return examName.match(/JOINT|Leg|Hand(-CT| MRI)|Forearm- CT|Femur|foot (CT|MRI)/);
     }
     function isAngio(examName) {
         const angioList = ['Celiac a three vessel', 'Lipiodol T.A.E.(trans-arterial embolization)-Lipiodol',
