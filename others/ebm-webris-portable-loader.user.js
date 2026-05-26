51cd1bd83877f08d9dd67feea4334d36687a8478
I-Ta Tsai @ Portable USB <ittsai@gmail.com>
2026-05-26 10:46:48 +0800

    feat: 新增 WebRIS portable loader
    - 新增 portable 環境專用的 Tampermonkey loader metadata。
    - 透過 file:///F:/Data/git/ahk-htch/others/ebm-webris.user.js 載入共用主程式。
    - 保留既有 WebRIS 網址比對、GM 權限與 localhost proxy 連線宣告。

---
+++ others/ebm-webris-portable-loader.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Enhanced WebRIS portable loader
+// @namespace    http://tsai.it/
+// @version      1
+// @description  Load Enhanced WebRIS from the portable repository checkout.
+// @author       I-Ta Tsai
+// @match        http://10.2.2.160:8080/
+// @match        http://10.2.2.160:8080/risworklist/*
+// @match        http://10.2.2.160:8080/verworklist/*
+// @match        https://hlwris.tzuchi.com.tw/webris/
+// @match        https://hlwris.tzuchi.com.tw/webris/risworklist/*
+// @match        https://hlwris.tzuchi.com.tw/webris/verworklist/*
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
+// @require      https://gist.githubusercontent.com/BrockA/2625891/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
+// @require      file:///F:/Data/git/ahk-htch/others/ebm-webris.user.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=2.160
+// @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
+// @connect      127.0.0.1
+// @connect      localhost
+// ==/UserScript==
